cd0e2b6750bdf54d6a118ba9bc614e4e3dbac494
taro31123 <natom779work@gmail.com>
2024-10-28 23:50:04 +0900

    add 課題001の内容を追加

--- kadai_001/js/script.js
+++ kadai_001/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  //   カルーセル
+  //  1カルーセル
   $(".carousel").slick({
     autoplay: true,
     dots: true,
@@ -10,7 +10,7 @@ $(function () {
     speed: 1000,
   });
 
-  // ボタンの不透明度 mouse on
+  // 2ボタンの不透明度 mouse on
   $(".home,.nav,.images").on("mouseover", function () {
     $(this).animate(
       {
@@ -29,7 +29,7 @@ $(function () {
     );
   });
 
-  //   TOPに戻るボタン
+  //  3TOPに戻るボタン
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > 100) {
       $(".top-btn").fadeIn();
@@ -38,7 +38,7 @@ $(function () {
     }
   });
 
-  //   ページ内リンクのスクロールをなめらかにする
+  //  4ページ内リンクのスクロールをなめらかにする
   $('a[href^="#"]').on("click", function (event) {
     event.preventDefault(); //デフォルトのリンク動作を防ぐ
     const target = $(this).attr("href"); //クリックしたときのhref属性を取得
@@ -50,5 +50,35 @@ $(function () {
     }
     const position = targetScroll.offset().top; //上からの位置を取得
     $("html, body").animate({ scrollTop: position }, 500, "swing");
+  });
+
+  //  5スクロールしたときにセクションをフェードインさせる
+  $(window).on("scroll", function () {
+    // スクロール位置とウィンドウの高さを取得
+    const scrollPos = $(window).scrollTop();
+    const windowHeight = $(window).height();
+
+    // フェードインさせたい各セクションをループで処理
+    $(".about, .works").each(function () {
+      const sectionTop = $(this).offset().top; // セクションの上端位置を取得
+      if (scrollPos > sectionTop - windowHeight + 100) {
+        // 調整用の100pxを追加
+        $(this).addClass("fade-in"); // クラス追加でフェードイン
+      }
+    });
+  });
+
+  //  6モーダル
+  // 画像がクリックされたときモーダル表示
+  $(".images").on("click", function () {
+    const imgSrc = $(this).attr("src"); //画像のsrcを取得
+    $(".modalImg").attr("src", imgSrc); // モーダルのimg要素のsrc属性に設定
+    $("#modal").fadeIn(); //モーダルの表示
+    $(".top-btn").hide(); //TOPにもどるボタンを隠す
+  });
+  // モーダルを閉じる
+  $("#modalClose").on("click", function () {
+    $("#modal").fadeOut();
+    $(".top-btn").show();
   });
 });
